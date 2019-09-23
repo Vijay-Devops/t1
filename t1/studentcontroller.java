43a7c8d647485f1cd91cd248566885089c155032
VijayViju <vijay1997may@gmail.com>
2019-09-23 13:22:07 +0530

    changes from vijay

--- t1/studentcontroller.java
+++ t1/studentcontroller.java
@@ -3,7 +3,8 @@ public class studentcontroller {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("student controller");
+		System.out.println("hjbhjgstudent controller new");
+		
 
 	}
 
