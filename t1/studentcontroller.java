ccbb463066aeca780e515c9503a01c267b094643
sriniwaz <niwaz841@gmail.com>
2019-09-23 15:58:35 +0530

    Update studentcontroller.java

--- t1/studentcontroller.java
+++ t1/studentcontroller.java
@@ -8,7 +8,7 @@ public class studentcontroller {
 		System.out.println("vignesh aaaaaaaaqqqqqqqqqqqqcontroller111fcd2");
 		System.out.println("vignesh gcontroller111fcd2");
 		System.out.println("vignesh controller111fcd21eeeee");
-		System.out.println("edited");
+		System.out.println("if you are bad im your dad");
 
 	}
 
