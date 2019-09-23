72af1e4ec1a65959fe49530cec46e2428f1aaeb0
VijayViju <vijay1997may@gmail.com>
2019-09-23 15:08:23 +0530

    changes from vijay

--- t1/studentcontroller.java
+++ t1/studentcontroller.java
@@ -4,9 +4,8 @@ public class studentcontroller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Enpt Vijay controller ");
-		System.out.println("vignesh controller ");
-
-
+		System.out.println("vignesh 11trtr111111controllexgdgxgr ");
+		System.out.println("changes from vijay");
 	}
 
 }
