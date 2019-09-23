7b2823cd347efeab2b6446ebd3b39d805c97d080
sriniwaz <niwaz841@gmail.com>
2019-09-23 13:42:02 +0530

    Update studentcontroller.java

--- t1/studentcontroller.java
+++ t1/studentcontroller.java
@@ -4,7 +4,8 @@ public class studentcontroller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Enpt Vijay controller ");
-		
+		System.out.println("vignesh controller ");
+
 
 	}
 
